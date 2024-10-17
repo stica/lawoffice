a5388562f28b8367b733a7b9d78027ea1b1be699
unknown <tica.srdjan@yahoo.com>
2024-10-17 21:27:46 +0200

    push

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
 
 const middleware = createMiddleware({
   // Add locales you want in the app
-  locales: ['en', 'sr'],
+  locales: ['sr', 'en'],
 
   // Default locale if no match
   defaultLocale: 'sr'
