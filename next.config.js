5008c5c70111ef6cefca37846bf829fa649bfe22
Srdjan Tica <tica.srdjan@yahoo.com>
2025-02-23 18:37:05 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,62 @@
-const nextTranslate = require('next-translate-plugin');
-const withNextIntl = require('next-intl/plugin')();
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
-  // For Static Export
-  // output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  // Enable production mode
+  productionBrowserSourceMaps: false,
+
+  // Enable minification
   swcMinify: true,
-  sassOptions: {
-    outputStyle: 'compressed'
-  }
+
+  // Configure Webpack
+  webpack: (config, { dev, isServer }) => {
+    // Only run in production client-side builds
+    if (!dev && !isServer) {
+      // Enable terser for JavaScript minification
+      config.optimization.minimize = true;
+      config.optimization.minimizer = config.optimization.minimizer || [];
+      
+      // Configure terser options
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              comparisons: false,
+              inline: 2,
+              drop_console: true,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
+          },
+        })
+      );
+
+      // Enable CSS minification
+      config.optimization.minimizer.push(
+        new CssMinimizerPlugin({
+          minimizerOptions: {
+            preset: ['default', {
+              discardComments: { removeAll: true },
+            }],
+          },
+        })
+      );
+    }
+
+    return config;
+  },
 }
 
-module.exports = withNextIntl({ ... nextConfig});
+module.exports = nextConfig;
