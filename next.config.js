b213d1d0616f6de644d27e92097286fdf6d5c5bd
Srdjan Tica <tica.srdjan@yahoo.com>
2025-02-23 18:50:06 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,62 +1,24 @@
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const nextTranslate = require('next-translate-plugin');
+const withNextIntl = require('next-intl/plugin')();
 
 // next.config.js
 const nextConfig = {
   // Enable production mode
   productionBrowserSourceMaps: false,
-
-  // Enable minification
+  
+  // Enable built-in SWC minification
   swcMinify: true,
-
-  // Configure Webpack
+  
+  // Basic webpack configuration
   webpack: (config, { dev, isServer }) => {
-    // Only run in production client-side builds
     if (!dev && !isServer) {
-      // Enable terser for JavaScript minification
       config.optimization.minimize = true;
-      config.optimization.minimizer = config.optimization.minimizer || [];
-      
-      // Configure terser options
-      config.optimization.minimizer.push(
-        new TerserPlugin({
-          terserOptions: {
-            parse: {
-              ecma: 8,
-            },
-            compress: {
-              ecma: 5,
-              warnings: false,
-              comparisons: false,
-              inline: 2,
-              drop_console: true,
-            },
-            mangle: {
-              safari10: true,
-            },
-            output: {
-              ecma: 5,
-              comments: false,
-              ascii_only: true,
-            },
-          },
-        })
-      );
-
-      // Enable CSS minification
-      config.optimization.minimizer.push(
-        new CssMinimizerPlugin({
-          minimizerOptions: {
-            preset: ['default', {
-              discardComments: { removeAll: true },
-            }],
-          },
-        })
-      );
     }
-
     return config;
   },
 }
 
-module.exports = nextConfig;
+
+module.exports = withNextIntl({ ... nextConfig});
